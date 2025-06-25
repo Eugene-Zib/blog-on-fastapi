41fbfaa4e936a9f53964de12183989b3deb8f3fe
Eugene-Zib <evgenzib@gmail.com>
2025-06-25 09:01:13 +0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "sqlite+aiosqlite:///./inpolium.db"
+DATABASE_URL = "sqlite+aiosqlite:///./database.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
